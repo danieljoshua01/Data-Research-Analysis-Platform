59ddfabd28dc6b0e71771f7708c33d6ea87e7fcc
danieljoshua01 <danieljoshua01@protonmail.com>
2025-02-23 15:55:38 +0500

    Added pm2 config file in the backend marketing foler.

--- backend-marketing/ecosystem.config.cjs
+++ backend-marketing/ecosystem.config.cjs
@@ -2,11 +2,11 @@ require('dotenv').config();
 module.exports = {
     apps: [
       {
-        name: 'DRA-Frontend-Marketing',
-        port: process.env.NUXT_PORT,
+        name: 'DRA-Backend-Marketing',
+        port: process.env.PORT,
         exec_mode: 'cluster',
         instances: 'max',
-        script: './.output/server/index.mjs'
+        script: 'ts-node src/index.ts'
       }
     ]
   }
